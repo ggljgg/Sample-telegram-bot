9acb69209ca1a0b371d8d5065d28e38b7480ac64
ggljgg <luidjy75@gmail.com>
2019-03-24 18:41:29 +0300

    Добавлена обработка команды /start и клавитур

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config');
 const helper = require('./helper');
+const keyboard = require('./keyboard-layout');
+const button = require('./keyboard-button');
 
 const bot = new TelegramBot(config.TOKEN, {polling: true});
 
 bot.on('message', msg => {
-    helper.logInConsole(msg);
+    // helper.logInConsole(msg);
+
+    switch(msg.text) {
+        case button.getHomeButtons().favourite:
+            break;
+        
+        case button.getHomeButtons().films:
+            bot.sendMessage(helper.getChatId(msg), 'Выберите жанр:', {
+                reply_markup: {
+                    keyboard: keyboard.getFilmLayout()
+                }
+            });
+            break;
+        
+        case button.getHomeButtons().cinemas:
+            break;
+        
+        case button.getBackButton():
+            bot.sendMessage(helper.getChatId(msg), 'Выберите команду для начала работы:', {
+                reply_markup: {
+                    keyboard: keyboard.getHomeLayout()
+                }
+            });
+            break;
+    }
 });
+
+bot.onText(/\/start/, msg => {
+    let text = `Здравствуйте, ${helper.getFirstName(msg)}!\nВыберите команду для начала работы:`;
+    bot.sendMessage(helper.getChatId(msg), text, {
+        reply_markup: {
+            keyboard: keyboard.getHomeLayout()
+        }
+    });
+});
+
+bot.on("polling_error", (err) => console.log(err));
 
 helper.logStart();

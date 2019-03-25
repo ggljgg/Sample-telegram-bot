8446661671539c92cf2e078dfd8c5fe74f048667
ggljgg <luidjy75@gmail.com>
2019-03-25 20:06:03 +0300

    Выполнен рефакторинг модуля index

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,23 @@
+const keyboard = require('./keyboard-layout');
+const Film = require('./models').Film;
+const Cinema = require('./models').Cinema;
+const geolib = require('geolib');
+const _ = require('lodash');
+
+function sendHTML(bot, chatId, html, keyboardType=null) {
+    let options = {
+        parse_mode: 'HTML'
+    };
+    
+    if (keyboardType) {
+        options['reply_markup'] = {
+            keyboard: keyboard.getKeyboardLayout(keyboardType)
+        };
+    }
+    
+    bot.sendMessage(chatId, html, options);
+}
+
 module.exports = {
     logStart() {
         console.log('Application was started...');
@@ -13,5 +33,31 @@ module.exports = {
 
     getChatId(message) {
         return message.chat.id;
+    },
+
+    sendFilmsByQuery(bot, chatId, query) {
+        Film.find(query).then(films => {
+            let html = films.map((f, i) => {
+                return f.name
+            }).join('\n');
+    
+            sendHTML(bot, chatId, html, 'film');
+        });
+    },
+
+    getCinemasInCoord(bot, chatId, location) {
+        Cinema.find({}).then(cinemas => {
+            cinemas.forEach(c => {
+                c.distance = geolib.getDistance(location, c.location) / 1000;
+            });
+            
+            cinemas = _.sortBy(cinemas, 'distance');
+    
+            let html = cinemas.map((c, i) => {
+                return `${c.name} - ${c.distance} км`
+            }).join('\n');
+            
+            sendHTML(bot, chatId, html, 'home');
+        });
     }
 };

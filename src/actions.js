29d79479d5e9ae873946700e9b453d2ca8819efc
ggljgg <luidjy75@gmail.com>
2019-04-24 21:59:34 +0300

    Перечисление вынесено в отдельный модуль

---
+++ src/actions.js
@@ -0,0 +1,12 @@
+const ACTION_TYPES = Object.freeze({
+    TOGGLE_FAV_FILM: 'tff',
+    SHOW_CINEMAS: 'sc',
+    SHOW_FILMS: 'sf',
+    SHOW_CINEMAS_ON_MAP: 'scom',
+    NEXT_PAGE: 'next',
+    PREV_PAGE: 'prev'
+});
+
+module.exports = {
+    ACTION_TYPES: ACTION_TYPES
+}
